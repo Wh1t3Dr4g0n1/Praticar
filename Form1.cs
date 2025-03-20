cd86f46128443966ee76583a2a2a3e48e86be70c
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-19 22:39:56 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace gluglu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(TextBox1.Text);
+            b = Convert.ToDouble(TextBox2.Text);
+            c = Convert.ToDouble(TextBox3.Text);
+            if (a == b && a == c && b == c)
+                MessageBox.Show("Triângulo Equilátero");
+            else if (a != b && a != c && b != c)
+                MessageBox.Show("Triângulo Escaleno");
+            else
+                MessageBox.Show("Triângulo Isóceles");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            TextBox1.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
