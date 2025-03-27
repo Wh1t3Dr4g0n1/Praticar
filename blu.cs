d6c7d9223d3e6ef1c74fcdb930665bd1006ffdd7
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-27 20:40:03 -0300

    blu.cs

--- blu.cs
+++ blu.cs
@@ -7,15 +7,47 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Vamos criar um questionário para saber se você consegue entrar para nossa escola.");
+
+
+
         int num;
         string brasil;
         string morse;
+        string cont;
 
-        Console.Write("me responda, qual o resultado de 4 x 4: ");
-        num = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+            Console.WriteLine("=================================================================================");
+            Console.WriteLine("Vamos criar um questionário para saber se você consegue entrar para nossa escola.");
+            Console.WriteLine("=================================================================================");
+            Console.Write("me responda, qual o resultado de 4 x 4: ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-        if (num == 16)
+            if (num == 16)
+            {
+                Console.WriteLine("Você acertou! Vamos para o próximo.+1");
+            }
+            else
+            {
+                Console.WriteLine("Você errou! vamos para o próximo. +0");
+            }
+            Console.WriteLine("=================================================================================");
+            Console.Write("Quem descobriu o Brasil: ");
+            brasil = Console.ReadLine();
+
+            if (brasil == "Pedro Álvares Cabral" || brasil == "Pedro Alvares Cabral" || brasil == "pedro alvares cabral" || brasil == "pedro álvares cabral")
+            {
+                Console.WriteLine("Você acertou! Vamos para o próximo.+1");
+            }
+            else
+            {
+                Console.WriteLine("Você errou! vamos para o próximo. +0");
+            }
+            Console.WriteLine("=================================================================================");
+            Console.Write("Quem criou o morse: ");
+            morse = Console.ReadLine();
+
+            if (morse == "Samuel Morse" || morse == "samuel morse")
         {
             Console.WriteLine("Você acertou! Vamos para o próximo.+1");
         }
@@ -23,28 +55,12 @@ public class HelloWorld
         {
             Console.WriteLine("Você errou! vamos para o próximo. +0");
         }
-        Console.Write("Quem descobriu o Brasil: ");
-        brasil = Console.ReadLine();
-
-        if (brasil == "Pedro Álvares Cabral")
-        {
-            Console.WriteLine("Você acertou! Vamos para o próximo.+1");
+        Console.WriteLine("Some os pontos se veja se chegou a 3. Se sim, você passou no teste!");
+            Console.WriteLine("=================================================================================");
+            Console.WriteLine("Quer recomeçar(S/N)?: ");
+        cont = Console.ReadLine();
         }
-        else
-        {
-        Console.WriteLine("Você errou! vamos para o próximo. +0");
-        }
-        Console.Write("Quem criou o morse: ");
-        morse = Console.ReadLine();
-
-        if (morse == "Samuel Morse")
-        {
-            Console.WriteLine("Você acertou! Vamos para o próximo.+1");
-        }
-        else
-        {
-            Console.WriteLine("Você errou! vamos para o próximo. +0");
-        }   
-            Console.Write("Some os pontos se veja se chegou a 3. Se sim, você passou no teste!");
+            while (cont == "s" || cont == "S" || cont == "sim" || cont == "Sim");
+            Console.ReadKey();
     }
 }
