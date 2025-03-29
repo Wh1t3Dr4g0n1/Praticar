395b44857f3dcc8316d6d2bc1cadef7044ac517d
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-28 21:16:01 -0300

    Login e registro.cs

---
+++ Login e registro.cs
@@ -0,0 +1,26 @@
+// Fazer um sistema de registro e login
+// pedir e-mail, usuário, e senha.
+// verificar se no login bate com o registrado.
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int 
+        string nome, rg, cpf;
+        Console.WriteLine("irei fazer umas perguntas de autenticação para saber se você faz parte desse sistema");
+        Console.Write("Seu nome: ");
+        nome = Console.ReadLine();
+        Console.Write("Seu RG: ");
+        rg = Console.ReadLine();
+        Console.Write("Seu CPF: ");
+        cpf = Console.ReadLine();
+        
+        
+        
+        
+        
+    }
+}
