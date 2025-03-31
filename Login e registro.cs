51fbe0c43d267e6c03a3cde1fb9e60726d0b2c99
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-31 19:16:21 -0300

    Login e registro.cs

--- Login e registro.cs
+++ Login e registro.cs
@@ -2,25 +2,68 @@
 // pedir e-mail, usuário, e senha.
 // verificar se no login bate com o registrado.
 
-using System;
 
-public class HelloWorld
-{
-    public static void Main(string[] args)
+
+
+    
+    string escolha, confirmar, confirmare, tentar;
+    string user, email, senha;
+    string usere, emaile, senhae;
+    Console.WriteLine("Bem-vindo, deseja se logar ou se registrar?");
+    Console.Write("(logar/registrar");
+    escolha = Console.ReadLine();
+
+    while (escolha == "registrar" || escolha == "Registrar" || escolha == "Registrar-se" || escolha == "registrar-se")
     {
-        int 
-        string nome, rg, cpf;
-        Console.WriteLine("irei fazer umas perguntas de autenticação para saber se você faz parte desse sistema");
-        Console.Write("Seu nome: ");
-        nome = Console.ReadLine();
-        Console.Write("Seu RG: ");
-        rg = Console.ReadLine();
-        Console.Write("Seu CPF: ");
-        cpf = Console.ReadLine();
-        
-        
-        
-        
-        
+        Console.Clear();
+        Console.WriteLine("===========");
+        Console.WriteLine("Registre-se");
+        Console.Write("Seu user: ");
+        user = Console.ReadLine();
+        Console.Write("Seu email: ");
+        email = Console.ReadLine();
+        Console.Write("Seu senha: ");
+        senha = Console.ReadLine();
+        Console.WriteLine("===========");
+        Console.Write("deseja se Registrar?");
+        confirmar = Console.ReadLine();
+    }
+
+    if (confirmar == "sim" || confirmar == "Sim" || confirmar == "S" || confirmar == "s" || escolha == "logar" || escolha == "Logar" || escolha == "logar-se" || escolha == "Logar-se" || escolha == "login" || escolha == "Login")
+    {
+        Console.Clear();
+        Console.WriteLine("===========");
+        Console.WriteLine("Logar-se");
+        Console.Write("User: ");
+        usere = Console.ReadLine();
+        Console.Write("Email: ");
+        emaile = Console.ReadLine();
+        Console.Write("Senha: ");
+        senhae = Console.ReadLine();
+        Console.WriteLine("===========");
+        Console.Write("Deseja confirmar login(S/N)?");
+        confirmare = Console.ReadLine();
+    }
+if (confirmare == "sim" && confirmare == "Sim" && confirmare == "S" && confirmare == "s")
+{
+    if (emaile == email && usere == user && senhae == senha)
+    {
+        Console.WriteLine("Seu login foi bem sucedido, Bem-vindo!");
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Seu login está incorreto, deseja tente novamente?");
+        Console.Write("(S/N): ");
+        tentar = Console.ReadLine();
+    }
+    if (tentar == "Não" || tentar == "não" || tentar == "n" || tentar == "N")
+    {
+        Console.Clear();
+        Console.WriteLine("===========");
+        Console.WriteLine("(aperte Enter até fechar...)");
+        Console.WriteLine("===========");
+        Console.ReadKey();
     }
 }
+    
